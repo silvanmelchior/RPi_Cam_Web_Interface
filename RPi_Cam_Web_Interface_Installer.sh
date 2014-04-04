51d22738b0fa63a5b3bce2ef3acf2a04fbae2b01
Salvatore La Bua <slabua@gmail.com>
2014-04-04 20:38:04 +0200

    Fix the particular case in which rpicamdir=""
    It avoids to add unwanted / in paths such as www//

--- RPi_Cam_Web_Interface_Installer.sh
+++ RPi_Cam_Web_Interface_Installer.sh
@@ -73,7 +73,11 @@ case "$1" in
         sudo mknod /var/www/$rpicamdir/FIFO p
         sudo chmod 666 /var/www/$rpicamdir/FIFO
 
-        sed -e "s/www/www\/$rpicamdir/" etc/apache2/sites-available/default.1 > etc/apache2/sites-available/default
+        if [ $rpicamdir == "" ] ; then
+                cat etc/apache2/sites-available/default.1 > etc/apache2/sites-available/default
+        else
+                sed -e "s/www/www\/$rpicamdir/" etc/apache2/sites-available/default.1 > etc/apache2/sites-available/default
+        fi
         sudo cp -r etc/apache2/sites-available/default /etc/apache2/sites-available/
         sudo chmod 644 /etc/apache2/sites-available/default
         sudo cp etc/apache2/conf.d/other-vhosts-access-log /etc/apache2/conf.d/other-vhosts-access-log
@@ -83,12 +87,20 @@ case "$1" in
         sudo chmod 755 /opt/vc/bin/raspimjpeg
         sudo ln -s /opt/vc/bin/raspimjpeg /usr/bin/raspimjpeg
 
-        sed -e "s/www/www\/$rpicamdir/" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
+        if [ $rpicamdir == "" ] ; then
+                cat etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
+        else
+                sed -e "s/www/www\/$rpicamdir/" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
+        fi
         sudo cp -r /etc/raspimjpeg /etc/raspimjpeg.bak
         sudo cp -r etc/raspimjpeg/raspimjpeg /etc/
         sudo chmod 644 /etc/raspimjpeg
 
-        sed -e "s/www/www\/$rpicamdir/" etc/rc_local_run/rc.local.1 > etc/rc_local_run/rc.local
+        if [ $rpicamdir == "" ] ; then
+                cat etc/rc_local_run/rc.local.1 > etc/rc_local_run/rc.local
+        else
+                sed -e "s/www/www\/$rpicamdir/" etc/rc_local_run/rc.local.1 > etc/rc_local_run/rc.local
+        fi
         sudo cp -r /etc/rc.local /etc/rc.local.bak
         sudo cp -r etc/rc_local_run/rc.local /etc/
         sudo chmod 755 /etc/rc.local
