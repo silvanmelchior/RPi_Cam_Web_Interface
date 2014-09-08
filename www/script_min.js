60dfb87813d79472f64703f4a2df80471f6ae3b6
Silvan Melchior <silvanmelchior@gmail.com>
2014-09-08 23:33:59 +0000

    Bugfix for clean.php

--- www/script_min.js
+++ www/script_min.js
@@ -2,9 +2,11 @@
 // MJPEG
 //
 var mjpeg_img;
+var halted = 0;
 
 function reload_img () {
-  mjpeg_img.src = "cam_pic.php?time=" + new Date().getTime();
+  if(!halted) mjpeg_img.src = "cam_pic.php?time=" + new Date().getTime();
+  else setTimeout("reload_img()", 500);
 }
 
 function error_img () {
