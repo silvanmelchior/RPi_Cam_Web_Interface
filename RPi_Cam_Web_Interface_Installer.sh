5a7985b921b94c028ed55bef0a01ff0dae46d9cd
Oleg Godovykh <olegg.lieangel@gmail.com>
2015-07-01 17:13:55 -0400

    Detect possibility to have dual camera during install

--- RPi_Cam_Web_Interface_Installer.sh
+++ RPi_Cam_Web_Interface_Installer.sh
@@ -460,6 +460,9 @@ case "$1" in
         else
           sed -e "s/www/www\/$rpicamdir/" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
         fi
+        if [ `cat /proc/cmdline |awk -v RS=' ' -F= '/boardrev/ { print $2 }'` == "0x11" ]; then
+          sed -i "s/^camera_num 0/camera_num 1/g" etc/raspimjpeg/raspimjpeg
+        fi
         if [ -e /etc/raspimjpeg ]; then
           $color_green; echo "Your custom raspimjpg backed up at /etc/raspimjpeg.bak"; $color_reset
           sudo cp -r /etc/raspimjpeg /etc/raspimjpeg.bak
@@ -562,6 +565,9 @@ case "$1" in
           sudo cat etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
         else
           sudo sed -e "s/www/www\/$rpicamdir/" etc/raspimjpeg/raspimjpeg.1 > etc/raspimjpeg/raspimjpeg
+        fi
+        if [ `cat /proc/cmdline |awk -v RS=' ' -F= '/boardrev/ { print $2 }'` == "0x11" ]; then
+          sed -i "s/^camera_num 0/camera_num 1/g" etc/raspimjpeg/raspimjpeg
         fi
         if [ -e /etc/raspimjpeg ]; then
           $color_green; echo "Your custom raspimjpg backed up at /etc/raspimjpeg.bak"; $color_reset
