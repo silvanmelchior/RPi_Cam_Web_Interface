395607da0fc06c79245e052203f41e36b6c97094
roberttidey <robert"tideys.net>
2016-08-11 15:44:47 +0100

    Add min.php simple preview only

---
+++ www/min.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>RPi Cam Preview</title>
+    <script src="js/script.js"></script>
+  </head>
+  <body onload="setTimeout('init(0,25,1);', 100);">
+    <center>
+      <div><img id="mjpeg_dest" /></div>
+    </center>
+ </body>
+</html>
