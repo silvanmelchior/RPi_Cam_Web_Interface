4b840bbb916df78f493fd0743a67472aa9a6ea0c
roberttidey <robert"tideys.net>
2016-08-15 10:02:21 +0100

    min.php is clickable to fullscreen

--- www/min.php
+++ www/min.php
@@ -3,10 +3,11 @@
   <head>
     <title>RPi Cam Preview</title>
     <script src="js/script.js"></script>
+	<link rel="stylesheet" href="css/style_minified.css" />
   </head>
   <body onload="setTimeout('init(0,25,1);', 100);">
     <center>
-      <div><img id="mjpeg_dest" /></div>
+      <div><img id="mjpeg_dest" onclick="toggle_fullscreen(this);" /></div>
     </center>
  </body>
 </html>
