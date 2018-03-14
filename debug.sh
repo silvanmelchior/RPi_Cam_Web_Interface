e9d8b02f2bd0a5923b3c0ed03b0c3e5a205f3a0d
roberttidey <robert"tideys.net>
2018-03-14 09:28:54 +0000

    Add debug script

---
+++ debug.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Copyright (c) 2015, Bob Tidey
+# All rights reserved.
+
+# Redistribution and use, with or without modification, are permitted provided
+# that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Neither the name of the copyright holder nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Description
+# This script stops a running RPi_Cam interface
+# Based on RPI_Cam_Web_Interface installer by Silvan Melchior
+# Edited by jfarcher to work with github
+# Edited by slabua to support custom installation folder
+# Additions by btidey, miraaz, gigpi
+# Split up and refactored by Bob Tidey 
+
+#Debug enable next 3 lines
+exec 5> start.txt
+BASH_XTRACEFD="5"
+set -x
+
+cd $(dirname $(readlink -f $0))
+
+source ./config.txt
+
+fn_stop ()
+{ # This is function stop
+   sudo killall raspimjpeg 2>/dev/null
+   sudo killall php 2>/dev/null
+   sudo killall motion 2>/dev/null
+}
+
+#start operation
+fn_stop
+sudo mkdir -p /dev/shm/mjpeg
+sudo chown www-data:www-data /dev/shm/mjpeg
+sudo chmod 777 /dev/shm/mjpeg
+sleep 1;sudo su -c 'raspimjpeg &' www-data
+if [ -e /etc/debian_version ]; then
+   sleep 1;sudo su -c "php /var/www/$rpicamdir/schedule.php > /dev/null &" www-data
+else
+   sleep 1;sudo su -c '/bin/bash' -c "php /var/www/$rpicamdir/schedule.php > /dev/null &" www-data
+fi
