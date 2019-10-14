3428f73219bf13ed325e75433d2099d0f60ce5e2
roberttidey <robert"tideys.net>
2019-10-14 12:29:13 +0100

    Allow video split to work with tim limited capture (ca 1 n)

--- www/config.php
+++ www/config.php
@@ -3,7 +3,7 @@
 	define('LBASE_DIR',dirname(__FILE__));
 	//Global defines and utility functions
 	// version string 
-	define('APP_VERSION', 'v6.6.5');
+	define('APP_VERSION', 'v6.6.6');
 
 	// name of this application
 	define('APP_NAME', 'RPi Cam Control');
