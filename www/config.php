6e6fc646b5ff47f1d8a0a70d193e48acd5003e12
roberttidey <robert@tideys.co.uk>
2021-09-28 22:58:25 +0100

    multiview now supports click through to camera settings from each image

--- www/config.php
+++ www/config.php
@@ -3,7 +3,7 @@
 	define('LBASE_DIR',dirname(__FILE__));
 	//Global defines and utility functions
 	// version string 
-	define('APP_VERSION', 'v6.6.17');
+	define('APP_VERSION', 'v6.6.18');
 
 	// name of this application
 	define('APP_NAME', 'RPi Cam Control');
--- www/multiview.php
+++ www/multiview.php
@@ -7,48 +7,51 @@ define('BASE_DIR', dirname(__FILE__));
 require_once(BASE_DIR.'/config.php');
 $multi = json_decode(file_get_contents("multiview.json"), true);
 
-if (isset($_GET["pHost"]))
-   {
-      $hostI = $_GET["pHost"];
-   } else {
-      $hostI = 0;
-   }
-$host = $multi["hosts"][$hostI];
-$preview_delay = $multi["delays"][$hostI];
+if (isset($_GET["rHost"])) {
+		$hostI = $_GET["rHost"];
+		$host = $multi["hosts"][$hostI];
+		header('Location: '.$host);
+	} else {
+	if (isset($_GET["pHost"])) {
+		$hostI = $_GET["pHost"];
+	} else {
+		$hostI = 0;
+	}
+	$host = $multi["hosts"][$hostI];
+	$preview_delay = $multi["delays"][$hostI];
 
-//writeLog("mjpeg stream with $preview_delay delay");
+	//writeLog("mjpeg stream with $preview_delay delay");
 
-// Used to separate multipart
-$boundary = "PIderman";
+	// Used to separate multipart
+	$boundary = "PIderman";
 
-// We start with the standard headers. PHP allows us this much
-header ("Content-type: multipart/x-mixed-replace; boundary=$boundary");
-header ("Cache-Control: no-cache");
-header ("Pragma: no-cache");
-header ("Connection: close");
+	// We start with the standard headers. PHP allows us this much
+	header ("Content-type: multipart/x-mixed-replace; boundary=$boundary");
+	header ("Cache-Control: no-cache");
+	header ("Pragma: no-cache");
+	header ("Connection: close");
 
-ob_flush();		//Push out the content we already have (gets the headers to the browser as quickly as possible)
+	ob_flush();		//Push out the content we already have (gets the headers to the browser as quickly as possible)
 
-set_time_limit(0); // Set this so PHP doesn't timeout during a long stream
+	set_time_limit(0); // Set this so PHP doesn't timeout during a long stream
 
-
-while(true) 
-{	
-	ob_start();
-	
-	echo "--$boundary\r\n";
-	echo "Content-type: image/jpeg\r\n";
-	
-	$fileContents = file_get_contents($host . 'cam_get.php');
-	$fileLength = strlen($fileContents);
-	
-	echo "Content-Length:" . $fileLength . "\r\n";
-	echo "\r\n";
-	
-	echo $fileContents;
-	
-	echo "\r\n";
-	ob_end_flush();
-	
-	usleep($preview_delay);
+	while(true) {
+		ob_start();
+		
+		echo "--$boundary\r\n";
+		echo "Content-type: image/jpeg\r\n";
+		
+		$fileContents = file_get_contents($host . 'cam_get.php');
+		$fileLength = strlen($fileContents);
+		
+		echo "Content-Length:" . $fileLength . "\r\n";
+		echo "\r\n";
+		
+		echo $fileContents;
+		
+		echo "\r\n";
+		ob_end_flush();
+		
+		usleep($preview_delay);
+	}	
 }
